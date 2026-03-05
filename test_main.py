01aec0309c0e09c9a6b794b666a4d3024f83f1a2
ahmetgunesceng1-alt <ahmetgunes.ceng1@gmail.com>
2026-03-05 20:22:39 +0300

    [KAN-34] create: test_main.py

---
+++ test_main.py
@@ -0,0 +1,11 @@
+import pytest
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_hello_endpoint():
+    response = client.get('/hello')
+    assert response.status_code == 200
+    assert response.json() == {'message': 'hello world'}
